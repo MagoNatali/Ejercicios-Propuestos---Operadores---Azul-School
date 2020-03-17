aee86cc07044bd480e1f22a5e09c8558248bb46c
MagoNatali <62276956+MagoNatali@users.noreply.github.com>
2020-03-17 01:30:31 -0500

    Create Propuesto04_Operadores.cpp

---
+++ Propuesto04_Operadores.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	double lado = 5.0,area1,area2;
+	area1 = lado*lado;
+	area2 = pow(lado,2);//Utilizando la Libretia Math.h
+	cout<<"El area del Cuadrado es: "<<area1<<" metros cuadrados"<<endl;
+	cout<<"El area del Cuadrado es: "<<area2<<" metros cuadrados"<<endl;
+	return 0;
+}
